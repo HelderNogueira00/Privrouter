3511f4f66a01b1ea0428b3411ba09e6ae11553d6
Helder Nogueira <heldernogueira.dev@gmail.com>
2025-11-10 10:17:00 -0500

    Vars.sh OK

--- bin/vars.sh
+++ bin/vars.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
+
+function get_val() {
+
+    conf_file="$1";
+    conf_param="$2";
+
+    if [ "${conf_file}" == "" ] || [ "${conf_param}" == "" ]; then 
+
+        echo "invalid arguments for get_val function";
+        return;
+    fi
+
+    conf_value="$(grep "^${conf_param}" ${conf_file} | cut -d '=' -f2)";
+    echo "${conf_value}";
+}
+
 ROUTING_TABLES='/etc/iproute2/rt_tables';
 PRIVROUTER_DIR='/etc/privrouter';
 
 LAN_CONF="${PRIVROUTER_DIR}/conf/lan.conf";
 MAIN_CONF="${PRIVROUTER_DIR}/conf/main.conf";
 
-TUNNELS=
+TUNNELS_CONF="${PRIVROUTER_DIR}/conf/tunnels/";
+CIRCUITS_CONF="${PRIVROUTER_DIR}/conf/circuits/";
+PROFILES_CONF="${PRIVROUTER_DIR}/conf/profiles/";
+RULESETS_CONF="${PRIVROUTER_DIR}/conf/rulesets/";
+NETWORKS_CONF="${PRIVROUTER_DIR}/conf/networks/";
+GATEWAYS_CONF="${PRIVROUTER_DIR}/conf/gateways/";
+
+#Load main configuration
+LAN_CIDR="$(get_val ${LAN_CONF} 'cidr')";
+LAN_IFNAME="$(get_val ${LAN_CONF} 'ifname')";
+MAIN_BOOTWAIT="$(get_val ${MAIN_CONF}, 'net_wait')";
+MAIN_VIRTUAL="$(get_val ${MAIN_CONF}, 'net_virtual')";
